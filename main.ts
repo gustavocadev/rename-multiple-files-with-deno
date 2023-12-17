df8e46ab07aace54541a0c3bbc09a4305c9ed68e
gustavocadev <gustavocadev@gmail.com>
2023-12-17 18:41:13 -0500

    chore: add deno.json

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-// @deno-types=npm:@types/inquirer
-import inquirer from 'npm:inquirer';
-import chalk from 'npm:chalk';
-import * as path from 'https://deno.land/std@0.164.0/path/mod.ts';
-// chalk - hello to my cli
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import * as path from 'std/path/mod.ts';
+
+// Hello, you're ready to start renaming!
 console.log(chalk.blue("Hello, you're ready to start renaming!"));
 
 const answers = await inquirer.prompt([
@@ -14,15 +14,17 @@ const answers = await inquirer.prompt([
 ]);
 
 const { address } = answers;
+
 // select of the files in the current directory
 const files = Deno.readDir(address.toString());
+
 let i = 1;
 // loop through the files
 for await (const file of files) {
   // check if the file is a file
   if (file.isFile && file.name !== 'main.ts') {
     // change the file name
-    const fileExtension = file.name.split('.').at(-1);
+    const fileExtension = path.extname(file.name);
     await Deno.rename(
       path.join(address, file.name),
       path.join(address, i + `.${fileExtension}`)
